b8c1176815f0aa37aa5ab0599bf4887915abbc82
pradipdhavan007 <96415156+pradipdhavan007@users.noreply.github.com>
2025-11-23 13:19:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to My First Java Project!");
+        System.out.println("This is uploaded on GitHub.");
+        
+        int a = 10, b = 20;
+        int sum = a + b;
 
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
